45938db5d466133e2651f2eb3421a21094b8bd08
vitaly89 <vitaly89@walla.com>
2017-02-21 23:10:23 +0200

    new-file-app.json

--- server.js
+++ server.js
@@ -5,14 +5,26 @@ const express = require('express');
 
 const path = require('path');
 
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+
+const compiler = webpack(webpackConfig);
+const webpackDevMiddleware = require('webpack-dev-middleware')(compiler, { noInfo: false, publicPath: webpackConfig.output.publicPath });
+
 const port = process.env.PORT || 8080;
 
 // const http = require('http');
 const app = express();
 
+
+app.use(webpackDevMiddleware);
+app.use(require('webpack-hot-middleware')(compiler));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 
+// if you want whole app to be available, you need webpack as well. this file should only serve api.
+// where is webpack configuration?
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
