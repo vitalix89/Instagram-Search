0d03f42f8fa2480085a2f13776878fb25d217fa7
vitaly89 <vitaly89@walla.com>
2017-02-22 00:04:19 +0200

    new-file-app.json

--- src/index.js
+++ src/index.js
@@ -7,21 +7,15 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import ReactDom from 'react-dom';
-// import optimistPromiseMiddleware from 'redux-optimist-promise';
+
 
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
-
-// import { composeWithDevTools } from 'remote-redux-devtools';
-// mport createLogger from 'redux-logger';
 
 
 import routes from './routes';
 
 
-// const logger = createLogger();
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(reducers,
     applyMiddleware(thunk)
   );
