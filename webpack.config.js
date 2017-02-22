7e42cb261824f4543526e3ee9fd3437550985398
vitaly89 <vitaly89@walla.com>
2017-02-22 11:10:15 +0200

    new-file-app.json

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   devtool: 'source-map',
   output: {
-    path: __dirname,
+    path: path.join(__dirname, '/dist/'),
     publicPath: '/',
     filename: 'bundle.js'
   },
