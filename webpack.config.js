ae6d8bce07357200c974eccf9b18e7ab728ac54b
vitaly89 <vitaly89@walla.com>
2017-02-22 11:28:51 +0200

    new-file-app.json

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,9 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: [
-
-    path.join('src', 'index.js')
+    './src/index.js'
   ],
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   output: {
     path: __dirname,
     publicPath: '/',
