452853817c3cd4de25dc38e293dc196e5d0b0a09
vitaly89 <vitaly89@walla.com>
2017-02-25 21:13:11 +0200

    new file update

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   devtool: 'source-map',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname),
     publicPath: '/',
     filename: 'bundle.js'
   },
