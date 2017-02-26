00db0e6190e9de260a941591755bae6dbed112fd
vitaly89 <vitaly89@walla.com>
2017-02-26 15:22:13 +0200

    update

--- server.js
+++ server.js
@@ -61,9 +61,9 @@ if (process.env.NODE_ENV !== 'production') {
   }));
 } else {
   app.use(express.static('public'));
-  // app.get('*', (request, response) => {
-  //   response.sendFile(path.join(__dirname, 'bundle.js'));
-  // / });
+  app.get('*', (request, response) => {
+     response.sendFile(path.join(__dirname, 'bundle.js'));
+   });
 
 
   app.get('/test', async (request, response) => {
