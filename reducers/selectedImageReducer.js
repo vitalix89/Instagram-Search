e9bd1ebf74ca9b0ad8263e9030fc1f54c2f094a7
vitaly89 <vitaly89@walla.com>
2017-02-27 18:17:32 +0200

    update

--- reducers/selectedImageReducer.js
+++ reducers/selectedImageReducer.js
@@ -7,7 +7,7 @@ export default (state = INITIAL_STATE, action) => {
 
       const img = action.payload.image;
 
-      console.log('SELECTED_IMAGE', action.payload);
+    //  console.log('SELECTED_IMAGE', action.payload);
       return { image: img };
     default:
       return state;
